b3ea2a52f0c2aa2cba65d7339412492c78452303
Samuel Medeiros <medeiros0441@hotmail.com>
2024-07-13 14:45:21 -0300

    13/07

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import routes from './routes/default';
+import Base from './components/Base';
+import { isAuthenticated } from './utils/auth';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Base>
+        <Routes>
+          {routes.map((route, index) => {
+            if (route.private) {
+              return (
+                <Route 
+                  key={index}
+                  path={route.path}
+                  element={isAuthenticated() ? <route.component /> : <Navigate to="/login" />}
+                />
+              );
+            } else {
+              return (
+                <Route 
+                  key={index}
+                  path={route.path}
+                  element={<route.component />}
+                />
+              );
+            }
+          })}
+        </Routes>
+      </Base>
+    </Router>
   );
-}
+};
 
 export default App;
--- setup/urls_admin.py
+++
@@ -1,6 +0,0 @@
-# myproject/urls_admin.py
-from django.urls import path
-
-urlpatterns = [
-    # Adicione outras rotas administrativas aqui
-]
